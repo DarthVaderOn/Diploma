123c4af8b9d4f47ceddcf66d4522973dc072d259
DarthWaderOn <mr.sokol913@yandex.ru>
2022-10-03 14:04:52 +0300

    heroku deploy

--- Bee/settings.py
+++ Bee/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['bee-online-shop-app.herokuapp.com']
 
 
 # Standard URL Login Required
