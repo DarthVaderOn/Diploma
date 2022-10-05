36994f0a09675f1f337f3407665bdd30df1085fe
DarthWaderOn <mr.sokol913@yandex.ru>
2022-10-05 10:29:14 +0300

    update registraion and authorization forms

--- profile_app/forms/authorization.py
+++ profile_app/forms/authorization.py
@@ -3,5 +3,5 @@ from django import forms
 
 class LoginForm(forms.Form):
     """Класс формы авторизации пользователя"""
-    username = forms.CharField(max_length=128)                      # имя
-    password = forms.CharField(widget=forms.PasswordInput())        # пароль
+    username = forms.CharField(min_length=3, max_length=128, widget=forms.PasswordInput(),)     # имя
+    password = forms.CharField(min_length=8, widget=forms.PasswordInput())                      # пароль
--- profile_app/forms/registration.py
+++ profile_app/forms/registration.py
@@ -6,10 +6,10 @@ from user_app.models import User
 class RegistrationForm(UserCreationForm):
     """Класс формы регистрации пользователя"""
 
-    username = forms.CharField()
+    username = forms.CharField(min_length=3, widget=forms.PasswordInput())
     email = forms.EmailInput()
-    password1 = forms.CharField(widget=forms.PasswordInput(), label='Enter password')
-    password2 = forms.CharField(widget=forms.PasswordInput(), label='Repeat password')
+    password1 = forms.CharField(min_length=8, widget=forms.PasswordInput(), label='Enter password')
+    password2 = forms.CharField(min_length=8, widget=forms.PasswordInput(), label='Repeat password')
 
     class Meta:
         """Вывод полей при регистрации"""
