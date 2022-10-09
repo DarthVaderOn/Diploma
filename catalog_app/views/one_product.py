d67a15e8a035ef8093228e53ca66e1988058162f
DarthWaderOn <mr.sokol913@yandex.ru>
2022-10-09 22:37:50 +0300

    update views for one_product: when an error is displayed: error and full information about the product, views and comments

--- catalog_app/views/one_product.py
+++ catalog_app/views/one_product.py
@@ -60,11 +60,31 @@ class ProductView(View):
         image_post = Media.objects.filter(post=one_product)
         reviews = Review.objects.order_by("-created_at").filter(post=one_product)
 
+        paginator = Paginator(reviews, 2)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
         rating = 0
         for review in reviews:
             rating += review.rating
         if rating > 0:
             rating = round(rating / len(reviews), 1)
+
+        media_post = []
+        if 'recently_viewed' in request.session:
+            if pk in request.session['recently_viewed']:
+                request.session['recently_viewed'].remove(pk)
+
+            recently_viewed_post = Post.objects.filter(pk__in=request.session['recently_viewed'])
+            request.session['recently_viewed'].insert(0, pk)
+            if len(request.session['recently_viewed']) > 5:
+                request.session['recently_viewed'].pop()
+
+            for post in recently_viewed_post:
+                media_post.append(post.media_post.first())
+        else:
+            request.session['recently_viewed'] = [pk]
+        request.session.modified = True
 
         if Review.objects.filter(post=one_product).filter(user=request.user).exists():
             return render(request, 'one_product.html', context={
@@ -73,6 +93,8 @@ class ProductView(View):
                 'reviews': reviews,
                 "rating": rating,
                 'form': form,
+                'recently_viewed_post': media_post,
+                'page_obj': page_obj,
                 'error': 'You have left a review before!'
             })
 
@@ -82,6 +104,8 @@ class ProductView(View):
                 'image_post': image_post,
                 'reviews': reviews,
                 'form': form,
+                'recently_viewed_post': media_post,
+                'page_obj': page_obj,
                 'error': 'Image upload error: Allowed upload limit is no more than 5 files!'
             })
 
