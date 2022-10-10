86079d3387aefe90c7e140f427f3db05508316a3
DarthWaderOn <mr.sokol913@yandex.ru>
2022-10-11 01:12:07 +0300

    updated one_product template: comment indents ; fixed search in profile api

--- profile_app/api/views/profile.py
+++ profile_app/api/views/profile.py
@@ -9,6 +9,6 @@ class ProfileView(GenericViewSet, ListModelMixin, RetrieveModelMixin):
     """Создаём класс вьюсет профиля"""
     serializer_class = ProfileSerializer
     queryset = Profile.objects.all()
-    filter_backends = [filters.OrderingFilter, filters.SearchFilter]
+    filter_backends = [filters.OrderingFilter, filters.SearchFilter ]
     permission_classes = [permissions.IsAuthenticated]                       # просмотр пользователей только для авторизованного пользователя
-    search_fields = ['username',]
+    search_fields = ['=id', 'user__username', 'user__first_name', 'user__last_name', 'about', 'phone']
